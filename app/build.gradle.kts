b861135c81ed40854ef6605eae0e259ff306e09d
vasmarfas <vasmarfas@mail.ru>
2026-09-25 00:58:33 +0300

    build: Bump default version name to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.firebase.perf)
 }
 
-val defaultVersionName = "1.4.4"
+val defaultVersionName = "1.5.0"
 
 val appVersionName: String = System.getenv("APP_VERSION_NAME") ?: defaultVersionName
 
